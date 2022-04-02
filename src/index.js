19f9c35ea437ef88f7b06946b3d4a4b11d1b9308
relara <relvira_7232@stu.southtexascollege.edu>
2022-04-02 10:42:46 -0500

    added logger

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import config from './utils/config'
+import logger from './utils/logger'
 
 const app = express()
 
+app.use(logger.middleware)
+
 app.get('/', (req, res) => {
+    logger.log.success('Calling root')
     res.send({ msg: 'hello there' })
 })
 
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
 import joi from 'joi'
 
+import logger from './logger'
+
 dotenv.config()
 
 const envSchema = joi
@@ -17,7 +19,7 @@ const { value: env, error } = envSchema.prefs({ errors:{ label:'key' }})
 .validate(process.env)
 
 if (error) {
-    throw new Error(`Config validation error: ${ error.message }`)
+    logger.log.error(new Error(`Config validation error: ${ error.message }`))   
 }
 
 export default { nodeEnv: env.NODE_ENV, port: env.PORT }
